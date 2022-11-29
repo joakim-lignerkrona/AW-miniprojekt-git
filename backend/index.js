918d44a9eff217ad8b0f62b45847e846014b40c2
Joakim Lignerkrona <jockan@icloud.com>
2022-11-29 11:57:19 +0100

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 // initiate express app
 const express = require('express');
 const app = express();
+// import path 
+const path = require('path');
+var bodyParser = require('body-parser')
+const __frontend = path.join(__dirname, '../frontend');
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+app.use(express.json());
 
 app.post('/api/newpost', (req, res) => {
     console.log(req.body);
@@ -13,4 +18,13 @@ app.post('/api/newpost', (req, res) => {
 app.post('/api/vote/:id', (req, res) => {
     console.log(req.body);
     res.send('POST request to the homepage');
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__frontend, 'index.html'));
+})
+app.get('/create', (req, res) => {
+    res.sendFile(path.join(__frontend, 'newTip.html'));
+})
+app.get('/post/:id', (req, res) => {
+    res.sendFile(path.join(__frontend, 'vote.html'));
 })
