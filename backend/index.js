5e13d4d2b6c4f4cb3646bb3df2913c0f7489c17e
Joakim Lignerkrona <jockan@icloud.com>
2022-11-29 11:15:04 +0100

    backend start

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// initiate express app
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
+app.post('/api/newpost', (req, res) => {
+    console.log(req.body);
+    res.send('POST request to the homepage');
+})
